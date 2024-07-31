06c48cd1df60f58dd9d295670f30d056fb4b84c3
Fry98 <filip-toman@seznam.cz>
2024-08-01 00:13:08 +0200

    Tweaks to entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,4 @@ RUN apk update && apk add openssl
 COPY ./scripts/entrypoint.sh ./temp.sh
 RUN cat temp.sh /usr/local/bin/docker-entrypoint.sh | tee /usr/local/bin/docker-entrypoint.sh > /dev/null
 RUN rm temp.sh
-ENTRYPOINT [ "docker-entrypoint.sh" ]
 CMD [ "postgres", "-c", "ssl=on", "-c", "ssl_cert_file=/var/lib/postgresql/server.crt", "-c", "ssl_key_file=/var/lib/postgresql/server.key" ]
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -f /var/lib/postgresql/server.key ]; then
-  openssl req -new -text -passout pass:qweasd -subj /CN=localhost -out server.req -keyout /var/lib/postgresql/privkey.pem
-  openssl rsa -in /var/lib/postgresql/privkey.pem -passin pass:qweasd -out /var/lib/postgresql/server.key
-  openssl req -x509 -in server.req -text -key /var/lib/postgresql/server.key -out /var/lib/postgresql/server.crt
+  openssl req  -nodes -new -x509 -subj /CN=localhost -keyout /var/lib/postgresql/server.key -out /var/lib/postgresql/server.crt
   chown 0:70 /var/lib/postgresql/server.key
   chmod 640 /var/lib/postgresql/server.key
 fi
